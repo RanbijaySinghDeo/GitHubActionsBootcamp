8ebbcade4e953bdc34a4ad7f258766e2077a4cb1
Ranbijay <ranbijaysinghdeo@gmail.com>
2026-07-04 14:28:28 +0530

    Initial Commit

---
+++ GitHubActionsBootcamp/GitHubActionsBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsBootcampApp.swift
+//  GitHubActionsBootcamp
+//
+//  Created by Ranbijay SinghDeo on 04/07/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
